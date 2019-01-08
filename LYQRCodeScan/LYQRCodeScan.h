995cef64b3985cf34d9bd18ae3210471ed35e326
gordon <liyang_xx@icloud.com>
2019-01-08 16:48:50 +0800

    Carthage

---
+++ LYQRCodeScan/LYQRCodeScan.h
@@ -0,0 +1,19 @@
+//
+//  LYQRCodeScan.h
+//  LYQRCodeScan
+//
+//  Created by 李扬 on 2019/1/8.
+//  Copyright © 2019 rrl360. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LYQRCodeScan.
+FOUNDATION_EXPORT double LYQRCodeScanVersionNumber;
+
+//! Project version string for LYQRCodeScan.
+FOUNDATION_EXPORT const unsigned char LYQRCodeScanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LYQRCodeScan/PublicHeader.h>
+
+
