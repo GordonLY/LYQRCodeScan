3e0d8cbeac2eaf9c3be8205022c1e04b2f928d81
gordon <liyang_xx@icloud.com>
2019-01-08 14:16:36 +0800

    fix image resources

---
+++ LYQRCodeScan/Bundle+LYScan.swift
@@ -0,0 +1,28 @@
+//
+//  Bundle+LYScan.swift
+//  LYQRCodeScanDemo
+//
+//  Created by 李扬 on 2019/1/8.
+//  Copyright © 2019 rrl360. All rights reserved.
+//
+
+import UIKit
+class ScanBundleClass { }
+extension Bundle {
+    
+    static var lyQRCodeBundle: Bundle? {
+        let bundle = Bundle(for: ScanBundleClass.self)
+        guard
+            let url = bundle.url(forResource: "ScanResource", withExtension: "bundle"),
+            let b = Bundle(url: url)
+            else { return nil }
+        return b
+    }
+    
+    static func getImage(byName name: String) -> UIImage? {
+        return UIImage.init(named: name, in: lyQRCodeBundle, compatibleWith: nil)
+    }
+}
+
+
+
